dc9c78cb22433b994f1466255424f909c0671402
Wissarut Kamto <i.unlimit3d@gmail.com>
2019-02-01 13:07:31 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -11,6 +11,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
+echo "Edite 001";
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
