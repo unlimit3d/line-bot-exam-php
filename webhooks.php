fad108eee0fd3cc37199ff016540b5a726606e23
Wissarut Kamto <i.unlimit3d@gmail.com>
2019-02-01 12:05:57 +0700

    edit access token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'WQB4l39UJUCOqRVcVlAZa2eeeYvxbzXxvyCOLxX9jvjYVqoPt2OFFThiILVVXyqQWNnvg2H+q/B6LpEIbgTasBIQbwHSHS9sAeysEAj6qWWnckuS472iFxs5nfAEqnM/l/hktVy/EgRZPvgxnas2lgdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
